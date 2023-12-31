87ce35ce708f645e8d9ac68b1fd8ed95af722f05
Henry Hartzler <hartzler.henry@gmail.com>
2023-12-31 15:32:24 -0700

    add initial state variables

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,14 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { View, Text } from 'react-native'
+import { firebaseAuth } from '../../firebaseConfig'
 
 const Login = () => {
+	const [email, setEmail] = useState<string | undefined>(undefined)
+	const [password, setPassword] = useState<string | undefined>(undefined)
+	const [loading, setLoading] = useState<string | undefined>(undefined)
+
+	const auth = firebaseAuth
+
 	return (
 		<View>
 			<Text>Login</Text>
 		</View>
 	)
 }
-
-
 
 export default Login
