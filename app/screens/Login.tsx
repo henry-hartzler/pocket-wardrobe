48217c099ea97fc71f9c1095eb630c32da9624da
Henry Hartzler <hartzler.henry@gmail.com>
2023-12-31 16:58:44 -0700

    extra padding for keyboard

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, ActivityIndicator } from 'react-native'
+import {
+	View,
+	StyleSheet,
+	ActivityIndicator,
+	KeyboardAvoidingView,
+} from 'react-native'
 import { Input, Button } from '@rneui/themed'
 import { firebaseAuth } from '../../firebaseConfig'
 import {
@@ -35,7 +40,7 @@ const Login = () => {
 				email,
 				password
 			)
-			alert('Check your e-mail')
+			alert('Sign-up successful')
 			console.log(response)
 		} catch (error: any) {
 			console.log(error)
@@ -47,39 +52,41 @@ const Login = () => {
 
 	return (
 		<View style={styles.container}>
-			<Input
-				placeholder='E-mail'
-				autoCapitalize='none'
-				value={email}
-				onChangeText={(text) => setEmail(text)}
-			/>
-			<Input
-				placeholder='Password'
-				autoCapitalize='none'
-				value={password}
-				secureTextEntry={true}
-				onChangeText={(text) => setPassword(text)}
-			/>
-
-			{loading ? (
-				<ActivityIndicator
-					size='large'
-					color='#0000ff'
+			<KeyboardAvoidingView behavior='padding'>
+				<Input
+					placeholder='E-mail'
+					autoCapitalize='none'
+					value={email}
+					onChangeText={(text) => setEmail(text)}
 				/>
-			) : (
-				<>
-					<Button
-						title='Login'
-						onPress={signIn}
-						disabled={email.length === 0 || password.length === 0}
+				<Input
+					placeholder='Password'
+					autoCapitalize='none'
+					value={password}
+					secureTextEntry={true}
+					onChangeText={(text) => setPassword(text)}
+				/>
+
+				{loading ? (
+					<ActivityIndicator
+						size='large'
+						color='#0000ff'
 					/>
-					<Button
-						title='Create account'
-						onPress={signUp}
-						disabled={email.length === 0 || password.length === 0}
-					/>
-				</>
-			)}
+				) : (
+					<>
+						<Button
+							title='Login'
+							onPress={signIn}
+							disabled={email.length === 0 || password.length === 0}
+						/>
+						<Button
+							title='Create account'
+							onPress={signUp}
+							disabled={email.length === 0 || password.length === 0}
+						/>
+					</>
+				)}
+			</KeyboardAvoidingView>
 		</View>
 	)
 }
