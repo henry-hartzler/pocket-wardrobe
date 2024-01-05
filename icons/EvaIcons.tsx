5c5c33118e95f0dc1710810b235521cac290abd1
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-01-05 15:47:02 -0700

    create icon file for exports

---
+++ icons/EvaIcons.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Icon, IconElement } from '@ui-kitten/components'
+
+export const PersonIcon = (props): IconElement => (
+	<Icon
+		{...props}
+		name='person-outline'
+	/>
+)
