9f12216c10eb60bb01896490ff590bdaaaeb0aa6
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-01-05 16:18:26 -0700

    added type Fit

--- types.ts
+++ types.ts
@@ -3,3 +3,13 @@ import { NavigationProp } from '@react-navigation/native'
 export type RouterProps = {
 	navigation: NavigationProp<any, any>
 }
+
+export type Fit = {
+	pants: string | undefined
+	top: string | undefined
+	blazer: string | undefined
+	cardigan: string | undefined
+	summer: boolean | undefined
+	winter: boolean | undefined
+	img: string | undefined
+}
