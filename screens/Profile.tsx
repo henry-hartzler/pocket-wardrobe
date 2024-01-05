c4fed9b7cb97721d3b3f21ccbd207310a3c48ec4
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-01-05 16:11:36 -0700

    buttons are all ui kitten now

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,19 +1,14 @@
 import { Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { firebaseAuth } from '../firebaseConfig'
-import { Button } from '@rneui/themed'
 import { RouterProps } from '../types'
-import { Layout } from '@ui-kitten/components'
-
+import { Layout, Button } from '@ui-kitten/components'
 
 const Profile = ({ navigation }: RouterProps) => {
 	return (
 		<Layout style={styles.container}>
 			<Text>Profile</Text>
-			<Button
-				onPress={() => firebaseAuth.signOut()}
-				title='Logout'
-			/>
+			<Button onPress={() => firebaseAuth.signOut()}>Logout</Button>
 		</Layout>
 	)
 }
