e36f4768f878f03cf4272cde1bb03ab5b8c48f8a
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-01-05 17:12:20 -0700

    added multi select for season

--- screens/AddNewOutfit.tsx
+++ screens/AddNewOutfit.tsx
@@ -1,11 +1,52 @@
-import { Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { Layout } from '@ui-kitten/components'
+import { Text, StyleSheet, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import { IndexPath, Layout, Select, SelectItem } from '@ui-kitten/components'
 
+const seasons = [
+	{
+		id: 0,
+		name: 'Summer',
+	},
+	{
+		id: 1,
+		name: 'Winter',
+	},
+]
 const AddNewOutfit = () => {
+	const [selectedIndex, setSelectedIndex] = useState<IndexPath | IndexPath[]>(
+		[]
+	)
+
 	return (
-		<Layout style={styles.container}>
-			<Text>AddNewOutfit</Text>
+		<Layout
+			style={styles.container}
+			level='1'
+		>
+			<SafeAreaView>
+				<Text>Add New Outfit</Text>
+				<Select
+					multiSelect={true}
+					selectedIndex={selectedIndex}
+					onSelect={(index) => setSelectedIndex(index)}
+					label='Season'
+					value={
+						selectedIndex[0]?.row === 0 && selectedIndex[1]?.row === 1
+							? 'Summer Winter'
+							: selectedIndex[0]?.row === 0
+							? 'Summer'
+							: selectedIndex[0]?.row === 1
+							? 'Winter'
+							: ''
+					}
+				>
+					{seasons.map((item) => (
+						<SelectItem
+							key={item.name + String(item.id)}
+							title={item.name}
+						/>
+					))}
+				</Select>
+			</SafeAreaView>
 		</Layout>
 	)
 }
@@ -13,8 +54,7 @@ const AddNewOutfit = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
+		minHeight: 128,
 	},
 })
 
