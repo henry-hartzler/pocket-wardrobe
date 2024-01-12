8227e6ba3ed1fd5c71a91cbfcce2940fac4e2f37
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-01-12 14:39:25 -0700

    remove signout function

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,14 +1,11 @@
 import { StyleSheet } from 'react-native'
 import React from 'react'
-import { firebaseAuth } from '../firebaseConfig'
-import { RouterProps } from '../types'
-import { Layout, Button, Text } from '@ui-kitten/components'
+import { Layout, Text } from '@ui-kitten/components'
 
-const Profile = ({ navigation }: RouterProps) => {
+const Profile = () => {
 	return (
 		<Layout style={styles.container}>
 			<Text>Profile</Text>
-			<Button onPress={() => firebaseAuth.signOut()}>Logout</Button>
 		</Layout>
 	)
 }
